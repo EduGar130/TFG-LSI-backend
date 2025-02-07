71ca8ac67a383b1b891f13a1048e7fe5a87106b8
Eduardo Garcia Romera <egaria3266@alumno.uned.es>
2025-02-07 21:51:12 +0100

    F3: Estructura back

---
+++ src/test/java/com/tfg/inventarioback/InventarioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.inventarioback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
